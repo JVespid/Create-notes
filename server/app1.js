aeb41c4affc7e7749a4b0fdc6e171ce1eb2c9849
JVespid <bernardo.abel.ls1@gmail.com>
2022-12-25 18:28:36 -0600

    implementando proyecto por completo

--- server/app1.js
+++
@@ -1,6 +0,0 @@
-const { createServer } = require('vite')
-const app = require('.')
-
-createServer(app).listen(3000, () => {
-  console.log('Servidor iniciado en el puerto 3000')
-})
--- server/routes/routeMain.js
+++ server/routes/routeMain.js
@@ -3,15 +3,15 @@ const path = require('path')
 const app = require('express').Router()
 
 app.get('/', async function (req, res) {
-  res.sendFile(path.join(__dirname, '/../../dist/index.html'))
+  res.sendFile(path.join(__dirname, '../../dist/index.html'))
 })
 
 app.get('/Notes', async function (req, res) {
-  res.sendFile(path.join(__dirname, '/../../dist/index.html'))
+  res.sendFile(path.join(__dirname, '../../dist/index.html'))
 })
 
 app.get('/Session', async function (req, res) {
-  res.sendFile(path.join(__dirname, '/../../dist/index.html'))
+  res.sendFile(path.join(__dirname, '../../dist/index.html'))
 })
 
 module.exports = app

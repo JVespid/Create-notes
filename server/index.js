19a3f28e39f2a720dfe2a6d1e0e0521135c40a3a
JVespid <bernardo.abel.ls1@gmail.com>
2023-01-14 18:56:00 -0600

    pruebas de despliege-4

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.static(publicPath));
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173/", "http://localhost:5173/", "https://notes-jvespid.vercel.app/"],
+    origin: "*",//["http://127.0.0.1:5173/", "http://localhost:5173/", "https://notes-jvespid.vercel.app/"],
   }),
 );
 

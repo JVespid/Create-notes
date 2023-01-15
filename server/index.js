afbbb6810d3c6c61228934bc0e3613f872837527
JVespid <bernardo.abel.ls1@gmail.com>
2023-01-14 20:39:19 -0600

    pruebas de despliege-8

--- server/index.js
+++ server/index.js
@@ -23,12 +23,6 @@ app.use(
   }),
 );
 
-server.use(
-  cors({
-    origin: "*",//["http://127.0.0.1:5173/", "http://localhost:5173/", "https://notes-jvespid.vercel.app/"],
-  }),
-);
-
 app.use("/", routeMain);
 
 io.on("connection", socket => {
